bb0492e1c5ac27fb5597f6bcbbf8bf04e5e886cb
Muhammadreza Haghiri <haghiri75@gmail.com>
2015-08-26 15:14:17 +0430

    All Files

---
+++ bin/MyClass.rb
@@ -0,0 +1,27 @@
+
+class MyClass #(change name)
+
+	include GladeGUI
+
+	def before_show()
+		@button1 = "+"
+	end	
+
+	def button1__clicked(*args)
+		result = @builder["entry1"].text.to_i + @builder["entry2"].text.to_i
+		@builder["label1"].label = result.to_s
+	end
+	def button2__clicked(*args)
+	result = @builder["entry1"].text.to_i * @builder["entry2"].text.to_i
+	@builder["label1"].label = result.to_s
+	end
+	def button3__clicked(*args)
+	result = @builder["entry1"].text.to_i - @builder["entry2"].text.to_i
+	@builder["label1"].label = result.to_s
+	end
+	def button4__clicked(*args)
+	result = @builder["entry1"].text.to_i / @builder["entry2"].text.to_i
+	@builder["label1"].label = result.to_s
+	end
+end
+
