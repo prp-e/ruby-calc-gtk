980cce7b1130c814a0c2a1ec9107b9b895164452
Muhammadreza Haghiri <haghiri75@gmail.com>
2015-08-26 21:29:14 +0430

    Formatted

--- bin/MyClass.rb
+++ bin/MyClass.rb
@@ -12,16 +12,16 @@ class MyClass #(change name)
 		@builder["label1"].label = result.to_s
 	end
 	def button2__clicked(*args)
-	result = @builder["entry1"].text.to_i * @builder["entry2"].text.to_i
-	@builder["label1"].label = result.to_s
+		result = @builder["entry1"].text.to_i * @builder["entry2"].text.to_i
+		@builder["label1"].label = result.to_s
 	end
 	def button3__clicked(*args)
-	result = @builder["entry1"].text.to_i - @builder["entry2"].text.to_i
-	@builder["label1"].label = result.to_s
+		result = @builder["entry1"].text.to_i - @builder["entry2"].text.to_i
+		@builder["label1"].label = result.to_s
 	end
 	def button4__clicked(*args)
-	result = @builder["entry1"].text.to_i / @builder["entry2"].text.to_i
-	@builder["label1"].label = result.to_s
+		result = @builder["entry1"].text.to_i / @builder["entry2"].text.to_i
+		@builder["label1"].label = result.to_s
 	end
 end
 
